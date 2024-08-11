69e21a6a5f63655765bfe637bd983e9b5394148c
Babatope Odunayo <Odunayo927@gmail.com>
2024-08-11 17:08:17 +0000

    updated the main file

--- main.tf
+++ main.tf
@@ -3,3 +3,13 @@ provider "aws" {
 	access_key = ""
 	secret_key = ""
 }
+
+resource "aws_vpc" "development-vpc" {
+	cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+	vpc_id = aws_vpc.development-vpc.id
+	cidr_block = "10.0.10.0/24"
+	availability_zone = "eu-west-3a"
+}

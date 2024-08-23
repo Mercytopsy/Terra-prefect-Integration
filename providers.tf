5037d940e82d11b55b848bf6693e9d582752a0a2
MercyBF <Babatopemercy@cndro.com>
2024-08-24 00:56:44 +0100

    added latest changes

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.64.0"
+    }
+
+    # linode = {
+    #   source = "linode/linode"
+    #   version = "2.26.0"
+    # }
+  }
+}
+
+

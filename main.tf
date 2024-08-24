b21484b3c7ec2d9cc86c4da52f623a49101fccce
MercyBF <Babatopemercy@cndro.com>
2024-08-25 00:30:13 +0100

    provision the vpc and subnet

--- main.tf
+++ main.tf
@@ -1,64 +1,24 @@
 provider "aws"{
     region = "eu-north-1" 
 }
-# variable "cidr_blocks" {
-#     description= "cidr blocks for vpc and subnet"
-#     #default= "" #This will execute automatically if a value isn't set
-#     type = list(string)
-# }
 
-# variable "cidr_blocks_object" {
-#     description= "cidr blocks for vpc and subnet"
-#     type = list(object({
-            # cidr_block = string
-            # name = string
-            # }))
-# }
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
-variable "subnet_cidr_block" {
-    description= "subnet cidr block"
-    #default= "" #This will execute automatically if a value isn't set
-    type = string
-}
-
-variable "vpc_cidr_block" {
-    description = "vpc cidr block"
-}
-
-variable "environment" {
-    description = "deployment environment"
-}
-
-resource "aws_vpc" "development-vpc"{
+resource "aws_vpc" "myapp-vpc"{
     cidr_block = var.vpc_cidr_block
-    # cidr_block = var.cidr_blocks[1]
-    # cidr_block = var.cidr_blocks_object[1].cidr_block
-
-    tags = {
-        Name: "development"
+    tags ={
+        Name: "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet-1"{
-    vpc_id = aws_vpc.development-vpc.id
+resource "aws_subnet" "myapp-subnet-1"{
+    vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block
-    # cidr_block = var.cidr_blocks[0]
-    # cidr_block = var.cidr_blocks_object[0].cidr_block
-    
-    availability_zone = "eu-north-1a"
+    availability_zone = var.avail_zone
     tags = {
-        Name: "subnet-dev"
+        Name: "${var.env_prefix}-subnet-1"
     }
-}
-
-# data "aws_vpc" "existing_vpc" {
-#     default = true
-# }
-
-output "dev-vpc-id"{
-    value = aws_vpc.development-vpc.id
-}
-
-output "dev-subnet-id"{
-    value = aws_subnet.dev-subnet-1.id
 }
--- terraform-dev.tfvars
+++ terraform-dev.tfvars
@@ -1,7 +1,4 @@
-subnet_cidr_block =  "10.0.10.0/24"
 vpc_cidr_block = "10.0.0.0/16"
-
-environment = "development"
-cidr_blocks = ["10.0.10.0/24", "10.0.0.0/16"]
-
-cidr_blocks_object = [{cidr_block= "10.0.10.0/24", name ="dev-subnet"}, {cidr_block = "10.0.0.0/16", name = "dev-vpc"}]
+subnet_cidr_block =  "10.0.10.0/24"
+avail_zone = "eu-north-1a"
+env_prefix = "dev"

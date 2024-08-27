94046a0c8ee94b73a3e3a88f6ab12c155b27f1c9
MercyBF <Babatopemercy@cndro.com>
2024-08-27 01:38:40 +0100

    use vpc module

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -1,6 +1,6 @@
-resource "aws_default_security_group" "default-sg" {
+resource "aws_security_group" "myapp-sg" {
     vpc_id = var.vpc_id
-
+    name = myapp-sg
     ingress {
         from_port = 22
         to_port = 22
@@ -56,7 +56,7 @@ resource "aws_instance" "myapp-server" {
 
     
     subnet_id = var.subnet_id
-    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
